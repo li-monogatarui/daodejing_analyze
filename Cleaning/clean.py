d93e2e9e77ef64804aa57a857d8b03301325581f
li-monogatarui <bingrui.ben.li@gmail.com>
2024-05-20 06:19:17 -0400

    Added more documents as well as docs2vec

--- Cleaning/clean.py
+++ Cleaning/clean.py
@@ -4,17 +4,17 @@ import nltk
 
 # Reading text file
 
-sites = ["addiss", "ChengLin", "legge", "lau", "YiWu", "mitchell", "hansen"]
+sites = ["addiss", "ChengLin", "legge", "lau", "YiWu", "mitchell", "hansen", "blakney", "tamgibbs", "ludd", "Lin", "Marshall"]
 
 # variables for each different site
 # i: choose specific version
-i = 6
+i = 11
 # first_str: choose first few words of text
-first_str = "To Guide with"
+first_str = "That which can be perceived"
 # offset: subtract a few characters to get first chapter number (1)
 offset = 7
 
-with open(f'Scraping/{sites[i]}.txt', 'r', encoding='utf8') as rf:
+with open(f'Scraping/text_files/{sites[i]}.txt', 'r', encoding='utf8') as rf:
     text = rf.read()
 new = re.sub(rf'[^a-zA-ZÀ-ÖÙ-öù-ÿĀ-žḀ-ỿ0-9 \']', ' ', text)
 
